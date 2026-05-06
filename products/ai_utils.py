10e5e60dd7fddb2bc2de0875b79848eb429dae46
Abdul Wasay <abdulwasayrao917@gmail.com>
2026-05-06 15:02:32 +0500

    embadding 2

--- products/ai_utils.py
+++ products/ai_utils.py
@@ -1,6 +1,5 @@
 import ollama
 import json
-from .models import Category, SubCategory
 
 def classify_product(product_name):
     response = ollama.chat(
@@ -8,44 +7,17 @@ def classify_product(product_name):
         messages=[{
             "role": "user",
             "content": f"""
-You are a strict JSON generator.
-
-Classify this product into ecommerce category.
+Return JSON only.
 
 Product: {product_name}
 
-Rules:
-- Only 1 category
-- Only 1 subcategory
-- No explanation
-- No extra text
-- Output must be valid JSON only
-
-Return format:
+Format:
 {{"category": "", "subcategory": ""}}
 """
         }]
     )
 
     try:
-        data = json.loads(response["message"]["content"])
+        return json.loads(response["message"]["content"])
     except:
-        data = {"category": "Other", "subcategory": "General"}
-
-    # -----------------------------
-    # DATABASE CHECK LOGIC
-    # -----------------------------
-
-    category_obj, created = Category.objects.get_or_create(
-        name=data["category"]
-    )
-
-    subcategory_obj, created = SubCategory.objects.get_or_create(
-        name=data["subcategory"],
-        category=category_obj
-    )
-
-    return {
-        "category": category_obj,
-        "subcategory": subcategory_obj
-    }
+        return {"category": "Other", "subcategory": "General"}
---
+++ products/embeddings.py
@@ -0,0 +1,28 @@
+from sentence_transformers import SentenceTransformer
+import numpy as np
+from .models import Category
+
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
+
+def find_best_category(product_name):
+    product_vec = model.encode(product_name)
+
+    best_cat = None
+    best_score = 0
+
+    for cat in Category.objects.all():
+        cat_vec = model.encode(cat.name)
+
+        similarity = np.dot(product_vec, cat_vec) / (
+            np.linalg.norm(product_vec) * np.linalg.norm(cat_vec)
+        )
+
+        if similarity > best_score:
+            best_score = similarity
+            best_cat = cat
+
+    if best_score >= 0.60:
+        return best_cat
+
+    return None
